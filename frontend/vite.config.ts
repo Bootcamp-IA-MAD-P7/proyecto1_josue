d4188d4b62e5138d86df8d759d30708ec92d77a8
Josue Diaz <josueac20@gmail.com>
2026-05-10 12:57:48 -0400

    remove variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,36 +1,32 @@
 import federation from '@originjs/vite-plugin-federation'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [
-      react(),
-      tailwindcss(),
-      federation({
-        name: 'taximetro_app', 
-        filename: 'remoteEntry.js', 
-        exposes: {
-          './TaximetroWidget': './src/App.tsx', 
-        },
-        shared: ['react', 'react-dom'] 
-      })
-    ],
-    server: {
-      port: 3002, 
-      host: true, 
-      watch: {
-        usePolling: true,
-      }
-    },
-    build: {
-      modulePreload: false,
-      target: 'esnext',
-      minify: false,
-      cssCodeSplit: false
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    federation({
+      name: 'taximetro_app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './TaximetroWidget': './src/App.tsx', 
+      },
+      shared: ['react', 'react-dom']
+    })
+  ],
+  server: {
+    port: 3002,
+    host: true,
+    watch: {
+      usePolling: true,
     }
+  },
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
   }
 })
